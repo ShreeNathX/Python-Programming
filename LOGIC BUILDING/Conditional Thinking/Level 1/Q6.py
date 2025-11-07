2dff9a506f322d64b77e52b40aa92bbaad1f73b3
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2025-11-07 23:40:09 +0530

    Added script to Take two numbers and print the larger one

---
+++ LOGIC BUILDING/Conditional Thinking/Level 1/Q6.py
@@ -0,0 +1,9 @@
+#Take two numbers and print the larger one
+
+a = int(input("Enter first num :"))
+b = int(input("Enter second num :"))
+
+if a>b:
+    print(f'{a} is greater')
+else:
+    print(f"{b} is greater")
