af93b5fee7828299cc3d9566534539d74c9f03a5
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2025-11-07 23:15:44 +0530

    Python

---
+++ LOGIC BUILDING/Conditional Thinking/Level 1/Q1.py
@@ -0,0 +1,10 @@
+#Take a number and print whether it’s positive, negative, or zero.
+
+n = int(input("Input :"))
+
+if n > 0:
+    print("Positive")
+elif n < 0:
+    print("Negative")
+else:
+    print("Zero")
---
+++ LOGIC BUILDING/Conditional Thinking/Level 1/Q2.py
@@ -0,0 +1,8 @@
+#Check if a number is even or odd.
+
+n = int(input("Input :"))
+
+if n%2 == 0:
+    print("even")
+else:
+    print("odd")
