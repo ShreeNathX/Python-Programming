6d44eccbf72039d41ebf581016c9f82d94b79aca
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-12 00:18:17 +0530

    Learning Python

---
+++ Basics/Control Statements/Jump Statement/Break.py
@@ -0,0 +1,4 @@
+for i in range(1,10):
+    if i == 5:
+        break       # It will stop the loop when it met a condition
+    print(i)
---
+++ Basics/Control Statements/Jump Statement/Pass.py
@@ -0,0 +1,4 @@
+for _ in range(1, 10):
+    if _ == 5:
+        pass            # Placeholder for future code
+    print(_)
