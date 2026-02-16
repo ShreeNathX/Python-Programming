fc14483ca1f295c9721bbbe3828d631556c9229c
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-17 01:33:17 +0530

    Learning Python

---
+++ Basics/Strings/Slicing.py
@@ -0,0 +1,11 @@
+text = "Python"
+
+# variable[start:end:steps]
+
+print(text[1:])                 # Output: ython
+print(text[:1])                 # Output: P
+print(text[2:4:1])              # Output: th
+print(text[2:5])                # Output: tho
+
+# reverse printing of string
+print(text[::-1])               # Output: nohtyP
