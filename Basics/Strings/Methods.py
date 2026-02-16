b3a8b43811606909f8f1af1b48c2dfe0cceaee57
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-17 01:17:06 +0530

    Learning Python

---
+++ Basics/Strings/Methods.py
@@ -0,0 +1,11 @@
+Str = "Python is the king."
+
+print(len(Str))                     # Provide the length of string
+print(Str.strip())                  # Remove white spaces from forward and backward of string
+print(Str.lower())                  # LowerCase
+print(Str.upper())                  # UpperCase
+print(Str.replace("Python", "AI"))  # Replace the string
+print(Str.startswith("Py"))         # True
+print(Str.endswith("ng."))          # True
+print(Str.capitalize())             # Make first character uppercase
+print(Str.title())                  # Make all first character which is before white space uppercase
