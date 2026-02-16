c14782471746d97112160a4b901750db166a0136
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-17 01:28:18 +0530

    Learning Python

--- Basics/Strings/Access-String.py
+++ Basics/Strings/Access-String.py
@@ -1,6 +1,12 @@
 text = 'Python'
 
-print(text[0])
+print(text[2])
 print(text[-1])
-print(text[0:3])
-print(text[::-2])
+
+print("Print in ascending order.")
+for _ in text:
+    print(_)
+
+print("Print in reverse order.")
+for _ in reversed(text):
+    print(_)
