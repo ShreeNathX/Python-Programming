53634e5cc0a303aa5e69650c677ce21e5cab84ac
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-21 00:44:30 +0530

    Vowel or consonant check

--- Small Programs/Phase 1/Level 1/char_type.py
+++ Small Programs/Phase 1/Level 1/char_type.py
@@ -1,3 +1,13 @@
 char = input("Enter any digit, char, special: ")
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower = "abcdefghijklmnopqrstuvwxyz"
 
-if char.isalpha
+if len(char) == 1:
+    if char is upper:
+        print(f'Uppercase character.')
+    elif char is lower:
+        print(f'Lowercase character.')
+    else:
+        print(f'Special character.')
+else:
+    print("")
