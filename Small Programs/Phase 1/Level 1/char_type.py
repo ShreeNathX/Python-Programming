0755442125d5e47674a3856fd021470e70956bdd
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-21 00:47:24 +0530

    Character type (upper/lower/digit/special) check

--- Small Programs/Phase 1/Level 1/char_type.py
+++ Small Programs/Phase 1/Level 1/char_type.py
@@ -1,13 +1,16 @@
 char = input("Enter any digit, char, special: ")
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower = "abcdefghijklmnopqrstuvwxyz"
+digit = "1234567890"
 
 if len(char) == 1:
     if char is upper:
         print(f'Uppercase character.')
     elif char is lower:
         print(f'Lowercase character.')
+    elif char is digit:
+        print(f'Digit.')
     else:
         print(f'Special character.')
 else:
-    print("")
+    print("Please provide only one character.")
