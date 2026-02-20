6ea9e7bf7488367198fbd95ffa1731414bd1e0e8
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-21 02:01:33 +0530

    Alphabet range (a–m or n–z)

---
+++ Small Programs/Phase 1/Level 2/alpha_range.py
@@ -0,0 +1,14 @@
+char = input("Enter only one character: ")
+
+a_m = "abcdefghijklm"
+n_z = "nopqrstuvwxyz"
+
+if len(char) == 1:
+    if char in a_m:
+        print(f'The range of "{char}" is in between a to m.')
+    elif char in n_z:
+        print(f'The range of "{char}" is in between n to z.')
+    else:
+        print(f'{char} is not an alphabet.')
+else:
+    print("Please provide only one character.")
