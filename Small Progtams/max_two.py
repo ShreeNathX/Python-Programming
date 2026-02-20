04af228ba3be3d9bbf5d6207146a14887c6b7ec6
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-20 23:05:36 +0530

    Larger in two numbers

---
+++ Small Progtams/max_two.py
@@ -0,0 +1,7 @@
+nums_1 = float(input("Enter first number: "))
+nums_2 = float(input("Enter second number: "))
+
+if nums_1 > nums_2:
+    print(f'{nums_1} is largest.')
+else:
+    print(f'{nums_2} is largest.')
