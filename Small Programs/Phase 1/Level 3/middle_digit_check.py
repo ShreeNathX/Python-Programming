888e00eea70a9a2a0a48ec1de9f116c64c22524f
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-22 01:44:13 +0530

    Middle digit largest/smallest/neither

--- Small Programs/Phase 1/Level 3/middle_digit_check.py
+++ Small Programs/Phase 1/Level 3/middle_digit_check.py
@@ -5,3 +5,14 @@ num_3 = input("Enter third digit: ")
 if num_1.isdigit() and num_2.isdigit() and num_3.isdigit():
     if len(num_1) == 1 and len(num_2) == 1 and len(num_3) == 1:
         num_1, num_2, num_3 = int(num_1), int(num_2), int(num_3)
+
+        if num_2 > num_1 and num_2 > num_3:
+            print(f"Middle digit {num_2} is greatest.")
+        elif num_2 < num_1 and num_2 < num_3:
+            print(f"Middle digit {num_2} is smallest.")
+        else:
+            print(f'Neither.')
+    else:
+        print("Please provide only one digit.")
+else:
+    print("Invalid input. Please enter digits only.")
