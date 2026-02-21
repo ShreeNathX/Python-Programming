1ee9f9b2cda3b36e83c4dde79b3d2edcb13a3973
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-22 00:54:59 +0530

    3-digit number with all distinct digits

--- Small Programs/Phase 1/Level 3/distinct_digits.py
+++ Small Programs/Phase 1/Level 3/distinct_digits.py
@@ -1,8 +1,16 @@
-num_1 = int(input("Enter first digit: "))
-num_2 = int(input("Enter second digit: "))
-num_3 = int(input("Enter third digit: "))
+num_1 = input("Enter first digit: ")
+num_2 = input("Enter second digit: ")
+num_3 = input("Enter third digit: ")
 
-if num_1 != num_2 and num_2 != num_3:
-    print(f"{num_1}, {num_2} and {num_3} are distinct.")
+
+if num_1.isdigit() and num_2.isdigit() and num_3.isdigit():
+    if len(num_1) == 1 and len(num_2) == 1 and len(num_3) == 1:
+        num_1, num_2, num_3 = int(num_1), int(num_2), int(num_3)
+        if num_1 != num_2 and num_2 != num_3 and num_1 != num_3:
+            print(f"{num_1}, {num_2}, and {num_3} are distinct.")
+        else:
+            print("Not distinct.")
+    else:
+        print("Please provide only one digit.")
 else:
-    print(f'Not distinct.')
+    print("Invalid input. Please enter digits only.")
