718047593a0bfb68ac105d29ac78050c3bb83757
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-24 21:19:06 +0530

    Number in range [100, 999]

---
+++ Small Programs/Phase 1/Level 3/range_100_999.py
@@ -0,0 +1,6 @@
+num = float(input("Enter any number: "))
+
+if num >= 100 and num < 1000:
+    print(f'{num} lies within the range [100, 999].')
+else:
+    print(f'{num} doesnot lie within the range [100, 999].')
