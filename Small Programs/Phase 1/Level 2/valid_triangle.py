ccf115425f1c034ee73e54c7d8623c7492b0a31f
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-25 22:40:14 +0530

    Valid triangle or not check

--- Small Programs/Phase 1/Level 2/valid_triangle.py
+++ Small Programs/Phase 1/Level 2/valid_triangle.py
@@ -2,7 +2,7 @@ x = int(input("Enter first side:"))
 y = int(input("Enter second side:"))
 z = int(input("Enter third side:"))
 
-if x > 0 and y > 0 and z > 0:
+if x > 0 and y > 0 and z > 0 and (x + y > z and y + z > x and z + x > y):
     print(f'Valid triangle.')
 else:
     print(f'Not valid triangle.')
