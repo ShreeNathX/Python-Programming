13f35373bc5a41bf717c64743f4c18a3d2d5c97e
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-28 23:48:10 +0530

    Numbers with even count of 1s in binary

--- Small Programs/Phase 2/Level 5/even_binary_ones.py
+++ Small Programs/Phase 2/Level 5/even_binary_ones.py
@@ -1,2 +1,8 @@
-i = max([5, 6, 9, 2])
-print(i)
+n = int(input("Enter n: "))
+
+for num in range(1, n + 1):
+    binary = bin(num)[2:]        # convert to binary (remove '0b')
+    ones_count = binary.count('1')
+
+    if ones_count % 2 == 0:
+        print(num, "->", binary)
