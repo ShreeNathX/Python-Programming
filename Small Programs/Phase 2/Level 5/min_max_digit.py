6c93ecca58cad527da8e54b07688e38786e4d10b
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-02-28 23:40:20 +0530

    Smallest and largest digit in a number

---
+++ Small Programs/Phase 2/Level 5/min_max_digit.py
@@ -0,0 +1,9 @@
+num = input("Enter any number at least greater than one digit: ")
+
+if len(num) < 2:
+    print("Please provide a two-digit number or greater.")
+else:
+    digits = [int(d) for d in num]  # convert each character to integer
+    greatest = max(digits)
+    smallest = min(digits)
+    print(f'{greatest} is greatest and {smallest} is smallest.')
