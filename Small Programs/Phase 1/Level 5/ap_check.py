2e78f7f95dcb9e56444acc527428ee859259651f
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-03 22:30:27 +0530

    Arithmetic progression check

---
+++ Small Programs/Phase 1/Level 5/ap_check.py
@@ -0,0 +1,9 @@
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+num_3 = int(input("Enter third number: "))
+
+
+if num_3 - num_2 == num_2 - num_1:
+    print("Yes they are in arithematic progession.")
+else:
+    print("No, they are not in are not in arithematic progression.")
---
+++ Small Programs/Phase 1/Level 5/clock_angle.py
@@ -0,0 +1,6 @@
+hours = int(input("Enter hour (1-12): "))
+minutes = int(input("Enter minute (1-60): "))
+
+dic = {
+    1 : 
+}
