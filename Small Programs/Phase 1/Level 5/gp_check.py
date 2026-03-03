9e75503307e2340294fe2885a1b4b0a193e12481
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-03 22:36:14 +0530

    Geometric progression check

---
+++ Small Programs/Phase 1/Level 5/gp_check.py
@@ -0,0 +1,8 @@
+num_1 = int(input("Enter first numbers: "))
+num_2 = int(input("Enter second numbers: "))
+num_3 = int(input("Enter third numbers: "))
+
+if num_3 // num_2 == num_2 // num_1:
+    print("Yes, they are in geometric progession.")
+else:
+    print("No, they are not in geometric progession.")
