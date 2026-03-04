f2a3490e12aa590f5ae156ece82dae13399aebf7
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-04 22:41:07 +0530

    Clock hands smaller angle

--- Small Programs/Phase 1/Level 5/clock_angle.py
+++ Small Programs/Phase 1/Level 5/clock_angle.py
@@ -1,6 +1,15 @@
-hours = int(input("Enter hour (1-12): "))
-minutes = int(input("Enter minute (1-60): "))
+h = int(input("Enter hours: "))
+m = int(input("Enter minutes: "))
 
-dic = {
-    1 : 
-}
+# Convert 12 to 0
+if h == 12:
+    h = 0
+
+minute_angle = 6 * m
+hour_angle = (30 * h) + (0.5 * m)
+
+angle = abs(hour_angle - minute_angle)
+
+smaller_angle = min(angle, 360 - angle)
+
+print("Smaller angle between hour and minute hands:", smaller_angle, "degrees")
