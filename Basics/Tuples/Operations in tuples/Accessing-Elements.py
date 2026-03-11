f32f1cf1792cd41e6fe3e5c004fd9bde9edc25c9
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-12 00:08:07 +0530

    Learning Python

---
+++ Basics/Tuples/Operations in tuples/Accessing-Elements.py
@@ -0,0 +1,12 @@
+# Access elements by position (indexing)
+
+t = (10, 20, 30, 40)
+print(t[0])
+print(t[-1])
+
+"""
+Output:
+10    - First Element as index is 0
+40    - Last Element as index is -1
+"""
+
