9a506f85f870ff40747f0b2b695c83aed582b1a8
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-12 00:02:03 +0530

    Learning Python

---
+++ Basics/Tuples/Ways to create tuples/Tuple-Constructor-Method.py
@@ -0,0 +1,18 @@
+t1 = tuple([8, 9, 3])       # From list
+t2 = tuple("abc")           # From String
+
+print(t1)
+print(type(t1))
+
+print(t2)
+print(type(t2))
+
+
+
+"""
+Output:
+(8, 9, 3)
+<class 'tuple'>
+('a', 'b', 'c')
+<class 'tuple'>
+"""
