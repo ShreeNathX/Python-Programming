2cf343f00231df751459822c0ffdd97b2f64a71e
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-19 00:18:02 +0530

    Learning Python

--- Basics/Dictionary/Adding & Updating/Update.py
+++ Basics/Dictionary/Adding & Updating/Update.py
@@ -4,11 +4,11 @@ d = {}
 d["name"] = "Ram"           # Add the element when empty
 
 # Update
-d["name"] = "Hari"          # Over ride the current element when not empty
+d["name"] = "Hari", 20      # Over ride the current element when not empty
 
 print(d)
 
 """
 Output:
-{'name': 'Hari'}
+{'name': ('Hari', 20)}
 """
