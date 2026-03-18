6d4cdd74bff07d7c5b2bfd2e91d07221857c09b2
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-19 00:44:20 +0530

    Learning Python

---
+++ Basics/Dictionary/Operations/Looping-Through-Dictionary.py
@@ -0,0 +1,23 @@
+d = {"a": 1, "b": 2}
+
+# Keys
+for key in d:
+    print(key)
+
+# Values
+for value in d.values():
+    print(value)
+
+# Key-Value pairs
+for k, v in d.items():
+    print(k, v)
+
+"""
+Output:
+a
+b
+1
+2
+a 1
+b 2
+"""
