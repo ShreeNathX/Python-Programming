a27afb85b3b09271241316cef74942db3a31084b
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-26 20:24:19 +0530

    Comparison

---
+++ DSA/String/Comparison.py
@@ -0,0 +1,12 @@
+s1 = "Apple"
+s2 = "Banana"
+
+# == Operator for Equality Check
+print(s1 == s2)
+
+# != Operator for Inequality Check
+print(s1 != s2)
+
+# Lexicographical Comparsion
+print(s1 < s2)
+print(s1 > s2)
