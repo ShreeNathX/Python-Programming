889ab71ef9c1d0b89c717a42ead6103c7ea636d9
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-04-02 12:58:03 +0530

    Learning Python

---
+++ Basics/Functions/CreatingFunction.py
@@ -0,0 +1,15 @@
+"""
+Syntax:
+def function_name(parameters):
+    ''' DocString Explaining Function'''
+    #Function Body
+    return result           # Optional
+
+Function Calling
+"""
+
+def greet(name):
+    return f'Hello {name}'
+
+print(greet('Alice'))
+
