0eacbe202f13b9d2bea9610a5eff1ded37d4b971
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-04-02 13:18:52 +0530

    Learning Python

---
+++ Basics/Functions/Return-Statement.py
@@ -0,0 +1,10 @@
+"""
+Functions can return values using return 
+If no return is given, Function returns None.
+"""
+
+def square(x):
+    return x**2
+
+print(square(5))            # We call the function and print the result
+
