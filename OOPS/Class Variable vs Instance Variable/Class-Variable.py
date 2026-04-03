8f854980ba3d95274bc7d37775e59668eef2ab4d
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-04-03 21:38:06 +0530

    Learning OOPS

---
+++ OOPS/Class Variable vs Instance Variable/Class-Variable.py
@@ -0,0 +1,18 @@
+class student:
+    college = "Sage University"
+
+    def __init__(self, name):
+        self.name = name
+    
+s1 = student("Ram")
+s2 = student("Sita")
+
+print(f'Student name is {s1.name}. College name is {s1.college}')
+print(f'Student name is {s2.name}. College name is {s2.college}')
+
+
+"""
+Output:
+Student name is Ram. College name is Sage University
+Student name is Sita. College name is Sage University
+"""
